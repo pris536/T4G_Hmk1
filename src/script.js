b43d5892c3473cac2f210cf41328c2e818dc1c70
pris536 <priscasiasebuabe@gmail.com>
2026-07-15 16:42:57 -0400

    added javascript

---
+++ src/script.js
@@ -0,0 +1,13 @@
+//name
+alert("Welcome to my website! Please answer the following questions.");
+
+let name = prompt("What is your name?");
+
+//age
+let age = prompt("How old are you?");
+
+//email
+let email = prompt("What is your email address?");
+
+let message = `Hello ${name}! You are ${age} years old and your email address is ${email}.`;
+alert(message);
